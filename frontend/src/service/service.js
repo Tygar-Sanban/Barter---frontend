64ce8cec4ac7762d1cfd052a86f7b76d793c5cbf
Bryan Tsanga <bryan-t@hotmail.fr>
2023-06-02 15:17:23 +0200

    some changes

--- frontend/src/service/service.js
+++ frontend/src/service/service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:5005",
+  baseURL:
+    "mongodb+srv://admin:s2OPtuiTrb6LwX4y@cluster0.j2x1pms.mongodb.net/BarterDB?retryWrites=true&w=majority",
 });
 
 service.interceptors.request.use((request) => {
