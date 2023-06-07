1e1a33373316d507dd7f54733f7d68a7da0f5f53
Romain Valls <romain.valls95@gmail.com>
2023-06-07 20:24:26 +0200

    CSS stuff

--- frontend/src/Components/SwitchComponent.jsx
+++ frontend/src/Components/SwitchComponent.jsx
@@ -2,9 +2,11 @@
 import React, { useState, useContext, useEffect } from "react";
 import service from "../service/service.js";
 import { AuthContext } from "../Context/authContext";
+import Switch from "@mui/material/Switch";
 
-function Switch() {
+function SwitchComponent() {
   const { user, isLoading, authenticateUser } = useContext(AuthContext);
+  const label = { inputProps: { "aria-label": "Switch demo" } };
 
   async function handleSwitch(event) {
     try {
@@ -19,16 +21,14 @@ function Switch() {
 
   return (
     <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={user.availability}
-          onChange={handleSwitch}
-        />
-        Availability: {user.availability ? "Available" : "Not Available"}
-      </label>
+      <Switch
+        checked={user.availability}
+        onChange={handleSwitch}
+        inputProps={{ "aria-label": "Switch demo" }}
+      />
+      Availability: {user.availability ? "Available" : "Not Available"}
     </div>
   );
 }
 
-export default Switch;
+export default SwitchComponent;
