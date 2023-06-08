4ba26db9f94024763df367e6988e5f1e915ffe1f
Romain Valls <romain.valls95@gmail.com>
2023-06-08 12:46:03 +0200

    styled profile, navbar dropdown, modify skills

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,27 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-      <navbar className="top-navbar">
-        <Link to="/request-page" className="bottom-navbar-component">
-          <div>Requests</div>
+    <div className="navbar-container">
+      <div className="menu-button">
+        <img
+          src="../../public/Icons/sidebar.png"
+          className="sidebar-icon"
+          onClick={toggleMenu}
+        />
+        <Link to="/profile">
+          <p>Profile</p>
         </Link>
-        <Link to="/current-missions" className="bottom-navbar-component">
-          <div>Current Services</div>
-        </Link>
-        <Link to="/search" className="bottom-navbar-component">
-          <div>Search</div>
-        </Link>
-        <Link
-          to="/profile"
-          className="bottom-navbar-component profilepicAndwallet"
-        >
-          <div>Profile</div>
-          <div>Wallet</div>
-        </Link>
-      </navbar>
+      </div>
+      <div className={`sidebar ${isMenuOpen ? "open" : ""}`}>
+        <div onClick={toggleMenu} className="sidebar-header">
+          Menu
+        </div>
+        <div className="sidebar-content">
+          <Link to="/request-page" className="sidebar-link">
+            Requests
+          </Link>
+          <Link to="/current-missions" className="sidebar-link">
+            Current Services
+          </Link>
+          <Link to="/search" className="sidebar-link">
+            Search
+          </Link>
+          <Link to="/profile" className="sidebar-link">
+            Profile
+          </Link>
+          <Link to="/wallet" className="sidebar-link">
+            Wallet
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
