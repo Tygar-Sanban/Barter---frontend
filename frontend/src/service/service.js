7150b440661729b07d87cfc67732d5c87f07c481
Romain Valls <romain.valls95@gmail.com>
2023-06-08 18:58:11 +0200

    changes for deployment

--- frontend/src/service/service.js
+++ frontend/src/service/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:5005",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 service.interceptors.request.use((request) => {
